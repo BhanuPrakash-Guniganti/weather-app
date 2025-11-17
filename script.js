27495f5478d5d61e1025aa2c1cb8c6a82e1ecd94
BhanuPrakash-Guniganti <gunigantibhanu@gmail.com>
2025-11-17 22:02:23 +0530

    Worked with js

---
+++ script.js
@@ -0,0 +1,20 @@
+const userLocation = document.getElementById("userLocation"),
+    conventer = document.getElementById("converter"),
+    weatherIcon = document.querySelector(".weatherIcon"),
+    temperature = document.querySelector(".temperature"),
+    feelsLike = document.querySelector(".feelsLike"),
+    description = document.querySelector(".description"),
+    date = document.querySelector(".date"),
+    city = document.querySelector(".city"),
+    HValue = document.getElementById("HValue"),
+    WValue = document.getElementById("WValue"),
+    SRValue = document.getElementById("SRValue"),
+    SSValue = document.getElementById("SSValue"),
+    CValue = document.getElementById("CValue"),
+    UVValue = document.getElementById("UVValue"),
+    PValue = document.getElementById("PValue"),
+    Forecast = document.querySelector(".Forecast");
+
+function findUserLocation() {
+    alert(1);
+}
