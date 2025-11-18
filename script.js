38a98145f6193fe6f0e6dd8bda623e7eca64f165
BhanuPrakash-Guniganti <gunigantibhanu@gmail.com>
2025-11-18 22:55:39 +0530

    Error solved in api

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-const userLocation = document.getElementById("userLocation"),
-    conventer = document.getElementById("converter"),
-    weatherIcon = document.querySelector(".weatherIcon"),
-    temperature = document.querySelector(".temperature"),
-    feelsLike = document.querySelector(".feelsLike"),
-    description = document.querySelector(".description"),
-    date = document.querySelector(".date"),
-    city = document.querySelector(".city"),
-    HValue = document.getElementById("HValue"),
-    WValue = document.getElementById("WValue"),
-    SRValue = document.getElementById("SRValue"),
-    SSValue = document.getElementById("SSValue"),
-    CValue = document.getElementById("CValue"),
-    UVValue = document.getElementById("UVValue"),
-    PValue = document.getElementById("PValue"),
-    Forecast = document.querySelector(".Forecast");
+const userLocation = document.getElementById ('userLocation'),
+  conventer = document.getElementById ('converter'),
+  weatherIcon = document.querySelector ('.weatherIcon'),
+  temperature = document.querySelector ('.temperature'),
+  feelsLike = document.querySelector ('.feelsLike'),
+  description = document.querySelector ('.description'),
+  date = document.querySelector ('.date'),
+  city = document.querySelector ('.city'),
+  HValue = document.getElementById ('HValue'),
+  WValue = document.getElementById ('WValue'),
+  SRValue = document.getElementById ('SRValue'),
+  SSValue = document.getElementById ('SSValue'),
+  CValue = document.getElementById ('CValue'),
+  UVValue = document.getElementById ('UVValue'),
+  PValue = document.getElementById ('PValue'),
+  Forecast = document.querySelector ('.Forecast');
 
-
- WEATHER_API_ENDPOINT =
-  "https://api.openweathermap.org/data/2.5/weather?q=Hyderabad&units=metric&appid=165fd8ca701ad0ed788a3f12ce7fbfa1";
+WEATHER_API_ENDPOINT =
+  "https://api.openweathermap.org/data/2.5/weather?q=";
 
 WEATHER_DATA_ENDPOINT =
-  "https://api.openweathermap.org/data/2.5/onecall?lat=17.3850&lon=78.4867&units=metric&appid=165fd8ca701ad0ed788a3f12ce7fbfa1";
+  "https://api.openweathermap.org/data/2.5/onecall?";
 
-  /*
-    WEATHER_API_ENDPOINT='https://api.openweathermap.org/data/2.5/weather?q=Hyderabad&units=metric&appid=165fd8ca701ad0ed788a3f12ce7fbfa1';
-    WEATHER_DATA_ENDPOINT='https://api.openweathermap.org/data/2.5onecal?appid=165fd8ca701ad0ed788a3f12ce7fbfa1&e';
-*/
+const API_KEY = "165fd8ca701ad0ed788a3f12ce7fbfa1";
+
+
 function findUserLocation() {
-    fetch(WEATHER_API_ENDPOINT + userLocation.value)
+  fetch(WEATHER_API_ENDPOINT + userLocation.value + "&units=metric&appid=" + API_KEY)
+
+
+  //  fetch(WEATHER_API_ENDPOINT + userLocation.value)
         .then((response) => response.json())
         .then((data) => {
             if (data.cod !== 200) {
@@ -36,6 +37,9 @@ function findUserLocation() {
             }
 
             console.log(data);
+
+            city.innerHTML = data.name + ", " + data.sys.country;
+            weatherIcon.computedStyleMap.background='url(https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png)';
 
             fetch(
                 `${WEATHER_DATA_ENDPOINT}lat=${data.coord.lat}&lon=${data.coord.lon}`
@@ -49,12 +53,3 @@ function findUserLocation() {
         })
         .catch((error) => console.error("Location Error:", error));
 }
-
-/*
-        fetch(WEATHER_DATA_ENDPOINT + '&lon=${data.coord.lon}&lat=${data.coord.lat}')
-        .then((response) => response.json())
-        console.log(data.coord.lon), data.coord.lat;
-    })
-    alert(1);
-}
-    */
