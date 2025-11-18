e33ef755ba8f48f1e02c8dcf3f47ba1632a24d10
BhanuPrakash-Guniganti <gunigantibhanu@gmail.com>
2025-11-18 23:47:58 +0530

    converted data and time stamp

--- script.js
+++ script.js
@@ -15,41 +15,84 @@ const userLocation = document.getElementById ('userLocation'),
   PValue = document.getElementById ('PValue'),
   Forecast = document.querySelector ('.Forecast');
 
-WEATHER_API_ENDPOINT =
-  "https://api.openweathermap.org/data/2.5/weather?q=";
+WEATHER_API_ENDPOINT = 'https://api.openweathermap.org/data/2.5/weather?q=';
 
-WEATHER_DATA_ENDPOINT =
-  "https://api.openweathermap.org/data/2.5/onecall?";
+WEATHER_DATA_ENDPOINT = 'https://api.openweathermap.org/data/2.5/onecall?';
 
-const API_KEY = "165fd8ca701ad0ed788a3f12ce7fbfa1";
+const API_KEY = '165fd8ca701ad0ed788a3f12ce7fbfa1';
 
+function findUserLocation () {
+  fetch (
+    WEATHER_API_ENDPOINT + userLocation.value + '&units=metric&appid=' + API_KEY
+  )
+    //  fetch(WEATHER_API_ENDPOINT + userLocation.value)
+    .then (response => response.json ())
+    .then (data => {
+      if (data.cod !== 200) {
+        alert (data.message);
+        return;
+      }
 
-function findUserLocation() {
-  fetch(WEATHER_API_ENDPOINT + userLocation.value + "&units=metric&appid=" + API_KEY)
+      console.log (data);
 
+      city.innerHTML = data.name + ', ' + data.sys.country;
+      weatherIcon.computedStyleMap.background =
+        'url(https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png)';
 
-  //  fetch(WEATHER_API_ENDPOINT + userLocation.value)
-        .then((response) => response.json())
-        .then((data) => {
-            if (data.cod !== 200) {
-                alert(data.message);
-                return;
-            }
+      fetch (
+        `${WEATHER_DATA_ENDPOINT}lat=${data.coord.lat}&lon=${data.coord.lon}`
+      )
+        .then (response => response.json ())
+        .then (data => {
+          console.log (data);
 
-            console.log(data);
+          temperature.innerHTML = data.current.temp;
+          feelsLike.innerHTML = 'Feels Like' + data.current.feels_like;
+          description.innerHTML =
+            `<i class="fa-brands fa-cloudversify"></i> &nbsp` +
+            data.current.weather[0].description;
 
-            city.innerHTML = data.name + ", " + data.sys.country;
-            weatherIcon.computedStyleMap.background='url(https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png)';
+          const options = {
+            weekday: "long",
+            month: "long",
+            day: "numeric", 
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+          };
 
-            fetch(
-                `${WEATHER_DATA_ENDPOINT}lat=${data.coord.lat}&lon=${data.coord.lon}`
-            )
-                .then((response) => response.json())
-                .then((weatherData) => {
-                    console.log(weatherData);
-                })
+          date.innerHTML =    'getLongFormateDateTime(data.current.dt, data.timezone_offset, options1)';
+           HValue.innerHTML= Math.round (data.current.humidity) + '<span>%</span>';
+          WValue.innerHTML =
+            Math.round (data.current.wind_speed) + '<span>m/s</span>';
+          const options1 = {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+          };
+          SRValue.innerHTML =
+            'getLongFormateDateTime(data.current.sunrise, data.timezone_offset, options1)';
+          SSValue.innerHTML = 'getLongFormateDateTime(data.current.sunset, data.timezone_offset, options1)';
+          CValue.innerHTML = data.current.clouds + '<span>%</span>';
+          UVValue.innerHTML = data.current.uvi;
+          PValue.innerHTML = data.current.pressure + '<span>hPa</span>';
+        });
+    });
+}
+
+function formatUnixTime (dtValue, offSet, options = {}) {
+  const date = new Date ((dtValue + offSet) * 1000);
+  return date.toLocaleTimeString ([], {timeZone: 'UTC', ...options});
+}
+
+function getLongFormateDateTime (dtValue, offSet, options) {
+  return formatUnixTime (dtValue, offSet, options);
+}
+
+/*         
                 .catch((error) => console.error("Weather Data Error:", error));
 
         })
         .catch((error) => console.error("Location Error:", error));
 }
+*/
